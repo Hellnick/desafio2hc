2f1164b27f1b5518cb3723e39bda59fa39d30e2b
Marco Aurélio <marco_reis92@hotmail.com>
2021-07-25 23:21:37 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/Hellnick/fakeAPI/db'
+  baseURL: 'https://my-json-server.typicode.com/Hellnick/fakeAPI/produtos'
 })
 
 export default api;
